5484afc3e3712b12936eab50b02368baf582318a
QuocDuy201103 <duyyduy123@gmail.com>
2024-10-12 18:12:29 +0700

    brand(delete), category (new catefory, edit category, delete category)

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Support\Str;
 use Intervention\Image\Laravel\Facades\Image;
 use Illuminate\Support\Facades\File;
@@ -16,6 +17,8 @@ class AdminController extends Controller
     {
         return view('admin.index');
     }
+
+    //brand
     public function brands()
     {
         $brands = Brand::orderBy('id', 'DESC')->paginate(10);
@@ -47,6 +50,8 @@ class AdminController extends Controller
         $brand->save();
         return redirect()->route('admin.brands')->with('status', 'Brand has been added succesfuly!');
     }
+
+
 
     public function edit_brand($id)
     {
@@ -80,6 +85,16 @@ class AdminController extends Controller
         return redirect()->route('admin.brands')->with('status', 'Brand has been updated succesfuly!');
     }
 
+    public function delete_brand($id){
+        $brand = Brand::find($id);
+        if(File::exists(public_path(('uploads/brands')) . '/' . $brand->image)){
+            File::delete(public_path(('uploads/brands')) . '/' . $brand->image);
+        }
+        $brand->delete();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been deleted succesfuly!');
+
+    }
+
     public function GenerateBrandThumbnailsImage($image, $imageName)
     {
         $destinationPath = public_path(('uploads/brands'));
@@ -89,4 +104,91 @@ class AdminController extends Controller
             $constrain->aspectRatio();
         })->save($destinationPath . '/' . $imageName);
     }
+
+    //category
+    public function categories(){
+        $categories = Category::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.categories', compact('categories'));
+    }
+
+    public function add_category()
+    {
+        return view('admin.category-add');
+    }
+
+
+    public function category_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $image = $request->file('image');
+        $file_extension = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+        $this->GenerateCategoryThumbnailsImage($image, $file_name);
+        $category->image = $file_name;
+        $category->save();
+        return redirect()->route('admin.categories')->with('status', 'Category has been added succesfuly!');
+    }
+
+    public function edit_category($id)
+    {
+        $category = Category::find($id);
+        return view('admin.category-edit', compact('category'));
+    }
+
+    public function update_category(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $category = Category::find($request->id);
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path(('uploads/categories')) . '/' . $category->image)) {
+                File::delete(public_path(('uploads/categories')) . '/' . $category->image);
+            }
+            $image = $request->file('image');
+            $file_extension = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp . '.' . $file_extension;
+            $this->GenerateCategoryThumbnailsImage($image, $file_name);
+            $category->image = $file_name;
+        }
+
+        $category->save();
+        return redirect()->route('admin.categories')->with('status', 'Category has been updated succesfuly!');
+    }
+
+    public function delete_category($id){
+        $category = Category::find($id);
+        if(File::exists(public_path(('uploads/categories')) . '/' . $category->image)){
+            File::delete(public_path(('uploads/categories')) . '/' . $category->image);
+        }
+        $category->delete();
+        return redirect()->route('admin.categories')->with('status', 'Category has been deleted succesfuly!');
+
+    }
+
+    public function GenerateCategoryThumbnailsImage($image, $imageName)
+    {
+        $destinationPath = public_path(('uploads/categories'));
+        $img = Image::read($image->path());
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function ($constrain) {
+            $constrain->aspectRatio();
+        })->save($destinationPath . '/' . $imageName);
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,23 @@ Route::middleware(['auth'])->group(function(){
 });
 
 Route::middleware(['auth',AuthAdmin::class])->group(function(){
+    //route brands
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
     Route::get('/admin/brands', [AdminController::class, 'brands'])->name('admin.brands');
     Route::get('/admin/brand/add', [AdminController::class, 'add_brand'])->name('admin.brand.add');
     Route::post('/admin/brand/store', [AdminController::class, 'brand_store'])->name('admin.brand.store');
     Route::get('/admin/brand/edit/{id}', [AdminController::class, 'edit_brand'])->name('admin.brand.edit');
     Route::put('/admin/brand/update', [AdminController::class, 'update_brand'])->name('admin.brand.update');
+    Route::delete('/admin/brand/delete{id}', [AdminController::class, 'delete_brand'])->name('admin.brand.delete');
+
+    ///route catogories
+    Route::get('/admin/categories', [AdminController::class, 'categories'])->name('admin.categories');
+    Route::get('/admin/category/add', [AdminController::class, 'add_category'])->name('admin.category.add');
+    Route::post('/admin/category/store', [AdminController::class, 'category_store'])->name('admin.category.store');
+    Route::get('/admin/category/edit/{id}', [AdminController::class, 'edit_category'])->name('admin.category.edit');
+    Route::put('/admin/category/update', [AdminController::class, 'update_category'])->name('admin.category.update');
+    Route::delete('/admin/category/delete{id}', [AdminController::class, 'delete_category'])->name('admin.category.delete');
+
+
 
 });
